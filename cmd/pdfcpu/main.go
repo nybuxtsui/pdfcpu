13776a7991b85d5297670207fc825d887fccaf71
徐彬 <nybux.tsui@gmail.com>
2022-06-22 16:45:22 +0800

    合并分割

--- cmd/pdfcpu/main.go
+++ cmd/pdfcpu/main.go
@@ -18,8 +18,15 @@ limitations under the License.
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"github.com/pdfcpu/pdfcpu/pkg/cli"
 )
 
 var (
@@ -35,6 +42,7 @@ var (
 var (
 	commit = "?"
 	date   = "?"
+	outDir = "out_temp"
 )
 
 func init() {
@@ -43,24 +51,114 @@ func init() {
 }
 
 func main() {
-	if len(os.Args) == 1 {
-		fmt.Fprintln(os.Stderr, usage)
+	conf, err := ensureDefaultConfig()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(0)
+	}
+	conf.OwnerPW = opw
+	conf.UserPW = upw
+	var inputReader *bufio.Reader = bufio.NewReader(os.Stdin)
+
+	if len(os.Args) > 2 {
+		// 合并
+
+		filesIn := []string{}
+		// 从第二个参数开始，是要合并的页数
+		for i := 1; i < len(os.Args); i++ {
+			filesIn = append(filesIn, os.Args[i])
+		}
+
+		// 打印出来，看看对不对
+		fmt.Println(filesIn)
+
+		outFile := "out.pdf"
+		// 合并
+		cmd := cli.MergeCreateCommand(filesIn, outFile, conf)
+		cli.Process(cmd)
+		fmt.Println("结束")
+		inputReader.ReadString('\n')
 		os.Exit(0)
 	}
 
-	// The first argument is the pdfcpu command string.
-	cmdStr := os.Args[1]
+	// 切分
+	// 入参，要切割的文件
+	inFile := os.Args[1]
+	// 校验文件是否存在
+	if conf.CheckFileNameExt {
+		ensurePDFExtension(inFile)
+	}
+	// 输出目录
+	os.RemoveAll(outDir)
+	os.MkdirAll(outDir, os.ModePerm)
 
-	// Process command string for given configuration.
-	str, err := cmdMap.process(cmdStr, "")
+	f, err := os.Open(inFile)
 	if err != nil {
-		if len(str) > 0 {
-			cmdStr = fmt.Sprintf("%s %s", str, os.Args[2])
+		log.Fatalln(err.Error())
+	}
+
+	func() {
+		defer func() {
+			if err != nil {
+				f.Close()
+				return
+			}
+			err = f.Close()
+		}()
+
+		api.Split(f, outDir, "out", 1, conf)
+	}()
+
+	fmt.Printf("输入需要的页(空格分隔, -表示连续)：")
+	input, err := inputReader.ReadString('\n')
+	if err == nil {
+		fmt.Printf("The input was: %s\n", input)
+	}
+	println(input)
+	items := strings.Split(input, " ")
+
+	// 合并
+
+	filesIn := []string{}
+	// 从第二个参数开始，是要合并的页数
+	for _, item := range items {
+		item = strings.Trim(item, " \r\n\t")
+		if strings.Contains(item, "-") {
+			ii := strings.Split(item, "-")
+			s, _ := strconv.Atoi(ii[0])
+			e, _ := strconv.Atoi(ii[1])
+			for i := s; i <= e; i++ {
+				filesIn = append(filesIn, outDir+"\\out_"+strconv.Itoa(i)+".pdf")
+			}
+		} else {
+			filesIn = append(filesIn, outDir+"\\out_"+item+".pdf")
 		}
-		fmt.Fprintf(os.Stderr, "%v \"%s\"\n", err, cmdStr)
-		fmt.Fprintln(os.Stderr, "Run 'pdfcpu help' for usage.")
+	}
+
+	// 打印出来，看看对不对
+	fmt.Println(filesIn)
+
+	outFile := "out.pdf"
+	// 合并
+	cmd := cli.MergeCreateCommand(filesIn, outFile, conf)
+	out, err := cli.Process(cmd)
+	if err != nil {
+		if needStackTrace {
+			fmt.Fprintf(os.Stderr, "Fatal: %+v\n", err)
+		} else {
+			fmt.Fprintf(os.Stderr, "%v\n", err)
+		}
 		os.Exit(1)
 	}
 
-	os.Exit(0)
+	if out != nil && !quiet {
+		for _, s := range out {
+			fmt.Fprintln(os.Stdout, s)
+		}
+	}
+
+	os.RemoveAll(outDir)
+
+	fmt.Println("结束")
+	inputReader.ReadString('\n')
 }
